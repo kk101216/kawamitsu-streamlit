c1df22a0466f4b3f21943d16e4daa47e3301451b
kk101216 <k_kwmt10@yahoo.co.jp>
2023-05-19 12:59:56 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import time
+
+st.title('kawamitu web')
+st.write('プログレスバーの表示')
+
+'start!!'
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i +1)
+    time.sleep(0.1)
+
+
+'Done!!'
